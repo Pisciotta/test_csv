2e59b9ef500f1f545fad3f684d93295bd4178eb0
Pisciotta <32421258+Pisciotta@users.noreply.github.com>
2022-12-25 23:07:53 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,45 +1,57 @@
 import pandas as pd
+import numpy as np
 from sklearn.metrics import precision_score
+import hyperopt
+from hyperopt import fmin, hp, tpe, STATUS_OK
 
-def function(df):
+'''
+Set your function's parameters
+'''
+params = {}
+params["N"] = [0,10,1] # Parameter "N" in the range 0 to 10 (min step = 1)
+params["margin"] = [0,10,0.1] # Parameter "margin" in the range 0 to 10 (min step 0.1)
+'''
+Set your class. E.g. True if next 5 candles close price drops below 10%,
+otherwise False.
+'''
+gain = -0.1
+next_candles = 5
+
+def function(df, p):
         '''
             Describe your function's logic here.
         '''
-        params = dict()
-        params["N"] = 20
-        params["margin"] = 0.9
+        # Get parameters and cast to correct types (if not float)
+        N,margin = p
+        N = int(N)
+
         # Initializing predictions as a boolean series:
-        predictions = pd.Series(data=True, index=df.index)
+        predictions = pd.Series(data=False, index=df.index)
         
         # Implement your logic after this line:
-        closeVolume = df[["close", "volume"]].values
-        for i, (price, volume) in enumerate(closeVolume):
-          if i < params["N"]:
-            continue
+        mean = df.close.rolling(N).mean()
+        std = df.close.rolling(N).std()
+        predictions = df.close > mean+margin*std
 
-          recentValues = closeVolume[i-params["N"]:i,:]
-          maxVolume = max(recentValues[:,1])
-          comparePrice = min([pastPrice for pastPrice, pastVolume in recentValues if pastVolume >= params["margin"]*maxVolume])
-
-          if price <= comparePrice:
-            predictions.iloc[i] = True
-        
         return predictions
-        
+
+
+"""
+NOT CHANGE AFTER THIS LINE
+"""
+def loss(predictions):
+  df["thresh"] = df.close*(1+gain)
+  df["real"] = df.close.rolling(next_candles).min().shift(-next_candles)
+  df["class"] = df["real"] < df["thresh"] if gain < 0 else df["real"] > df["thresh"]
+  score = int(precision_score(df["class"], predictions)*100) if sum(predictions) > 0 else 0
+  return -score
+
 if __name__ == "__main__":
-  gain = -0.1
-  next_candles = 5
-  scores = []
+  scores, space = [], []
+  for key in params:
+    space.append(hp.quniform(key, params[key][0], params[key][1], params[key][2]))
   for i, fName in enumerate(["BBIG", "PIXY", "RELI", "SXTC", "WEI"]):
     df = pd.read_csv(filepath_or_buffer="https://raw.githubusercontent.com/Pisciotta/test_csv/main/"+fName+".csv")
-    predictions = function(df)
-    df["thresh"] = df.close*(1+gain)
-    df["real"] = df.close.rolling(next_candles).min().shift(-next_candles)
-    if gain < 0:
-      df["class"] = df["real"] < df["thresh"]
-    else:
-      df["class"] = df["real"] > df["thresh"]
-    score = int(precision_score(df["class"], predictions)*100) if sum(predictions) > 0 else 0
-    print("Score",i,":", score,"%")
-    scores.append(score)
-  print("---------\nMean Value:", sum(scores)/len(scores), "%")
+    fn = lambda x: loss(function(df,x))
+    min_params = fmin(fn=fn, space=space, algo=tpe.suggest, max_evals=500)
+    scores.append(min_params)
