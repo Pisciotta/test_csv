413adc7b56f50dad2858ed3c3453cba6b2468667
Pisciotta <32421258+Pisciotta@users.noreply.github.com>
2022-12-26 00:10:46 +0100

    Update test.py

--- test.py
+++ test.py
@@ -8,9 +8,9 @@ Set your function's parameters
 '''
 params = {}
 params["N"] = [0,15,1] # Parameter "N" in the range 0 to 10 (min step = 1)
-params["margin"] = [0,5,1] # Parameter "margin" in the range 0 to 10 (min step 0.1)
+params["margin"] = [0,5,0.1] # Parameter "margin" in the range 0 to 5 (min step 0.1)
 '''
-Set your class. E.g. True if next 5 candles close price drops below 10%,
+Set your class. E.g. True if next 7 candles close price drops below 10%,
 otherwise False.
 '''
 gain = -0.1
